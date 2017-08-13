b1f73f044ac6bc864475f8bc13a322d3f447bb13
senenkovitalik <senenkovitalik@gmail.com>
2017-08-13 14:08:27 +0300

    fix: Popover close behavior

--- js/product-item/product-item.my-popover.directive.js
+++ js/product-item/product-item.my-popover.directive.js
@@ -5,7 +5,7 @@ angular.module('myShoppingList')
     link: (scope, element) => {
       const container = angular.element('<div />');
       // const template = '<div class=\'popover\'><div class=\'popover-content\'></div></div>';
-      const content = 'Are you sure?<br><button class=\'btn btn-primary buy popover-hide\' ng-disabled=\'inBasket(x)\' ng-click=\'buy(x)\'>OK</button><button class=\'btn btn-default popover-hide\'>Cancel</button>';
+      const content = 'Are you sure?<br><button class=\'btn btn-primary popover-hide\' ng-disabled=\'inBasket(x)\' ng-click=\'buy(x)\'>OK</button><button class=\'btn btn-default popover-hide\'>Cancel</button>';
 
       $(container).append(content);
       $compile(container)(scope);
@@ -20,9 +20,21 @@ angular.module('myShoppingList')
         elem.popover('toggle');
       });
 
+      function hidePopover() {
+        elem.popover('hide');
+      }
+
+      function detachListener() {
+        $('body').off('click', hidePopover());
+      }
+
       elem.on('shown.bs.popover', () => {
-        $('.popover-hide').on('click', (e) => {
-          elem.popover('hide');
+        $('.popover-hide').on('click', () => {
+          hidePopover();
+        });
+        $('body').on('click', () => {
+          hidePopover();
+          detachListener();
         });
       });
 
